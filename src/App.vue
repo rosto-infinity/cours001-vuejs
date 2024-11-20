4b0ef13c802916f291bac29e4c7b6df7ac220c6c
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-20 19:28:48 +0100

    dbd01

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 
 <template>
   <h1 class="text-3xl  text-red-500 font-bold underline">
-    Hello world!
+    Hello world! rosto
   </h1>
+  <p class="text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate repellendus blanditiis cumque, adipisci omnis magnam tenetur ut laudantium similique quidem praesentium rerum voluptatibus eaque corporis ullam molestiae consectetur perspiciatis eveniet nulla beatae cupiditate qui dolorem quis hic? Mollitia inventore quas reprehenderit magni quibusdam ad asperiores animi omnis odit, velit praesentium.</p>
 </template>
